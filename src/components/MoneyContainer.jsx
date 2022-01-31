b525fc968338ea2bd3c098a6c3608e57454e7567
Sw0xy <aliemreali5@.com>
2022-01-31 15:11:43 +0300

    unused import removed

--- src/components/MoneyContainer.jsx
+++ src/components/MoneyContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { errorSelector, moneySelector } from '../redux/moneySlice';
 import { Typography } from 'antd';
